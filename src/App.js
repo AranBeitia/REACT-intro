909871cf8971f36290d0b01947f139cbce4247d8
AranBeitia <arantza.beitia@gmail.com>
2021-09-18 13:59:08 +0200

    state and localstorage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+// core libraries
+import React, { Component } from 'react'
+// third party libraries
+// custom
+import { readLocalStorage, writeLocalStorage } from './utils/localStorage'
+import './App.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			user: {
+				firstName: null,
+				lastName: null,
+				email: null,
+				isLoggedIn: false,
+			},
+			count: 0,
+		}
+
+    this.increment = this.increment.bind(this)
+    this.login = this.login.bind(this)
+    this.logout = this.logout.bind(this)
+	}
+
+  increment () {
+    this.setState(prevState => ({ count: prevState.count + 1}))
+  }
+
+  login() {
+    this.setState(prevState => ({
+      ...prevState,
+      user: {
+				firstName: 'Aran',
+				lastName: 'Beitia',
+				email: 'ab@holi.com',
+				isLoggedIn: true,
+			}
+    }))
+  }
+
+  logout() {
+    this.setState({
+      user: {
+				firstName: null,
+				lastName: null,
+				email: null,
+				isLoggedIn: false,
+			},
+    })
+  }
+
+  componentDidMount() {
+    const lastState = readLocalStorage("app-state")
+    this.setState({ 
+      // user: lastState.user,
+      // count: lastState.count // es lo mismo que la siguiente linea
+      ...lastState
+     })
+    console.log(lastState);
+    console.log('mounted');
+  }
+
+  componentDidUpdate() {
+    writeLocalStorage('app-state', JSON.stringify(this.state))
+    console.log('updated');
+  }
+
+  // componentWillUnmount() {
+  //   console.log('unmounted');
+  // }
+
+	render() {
+		return (
+			<main className="App">
+				<p>Counter: {this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.login}>login</button>
+        <button onClick={this.logout}>logout</button>
+        <div>
+          {this.state.user.firstName && <p>{this.state.user.firstName}</p>}
+        </div>
+			</main>
+		)
+	}
 }
 
-export default App;
+export default App
